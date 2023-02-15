431566e77eef0418cb804b78ac7c3c37a0be6c1d
Ensar Uysal <eensaruysal@gmail.com>
2023-02-16 00:17:22 +0300

    Workspace edited

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is started on ${port}`);
+});
+ 
