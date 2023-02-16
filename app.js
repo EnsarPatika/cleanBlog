d50dbb83223cf271ad9082bba5771e55349895d6
Ensar Uysal <eensaruysal@gmail.com>
2023-02-17 01:35:43 +0300

    ejs exersize is done

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
-app.get('/', (req, res) => {
-  res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
-});
 
+app.set('view engine', 'ejs');
+app.use(express.static('public')); 
+
+app.get('/', (req, res) => {
+  // res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  // res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
+  res.render('about')
+});
+app.get('/add_post', (req, res) => {
+  // res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
+  res.render('add_post')
+});
+app.get('/index', (req, res) => {
+  // res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
+  res.render('index')
+});
+app.get('/post', (req, res) => {
+  // res.send({ id: 1, title: 'Blog title', description: 'Blog description' });
+  res.render('post')
+});
 const port = 3000;
 
 app.listen(port, () => {
